ac9943d68e942ef03e37ef6183b5a9a22779668a
Trey Bianchini <trey.bianchini@iinteractive.com>
2020-06-05 10:05:01 -0500

    Basic String Component
    display MinLength, MaxLength and regex pattern
    display defaults if not furnished in input
    flag invalid numeric values

--- src/components/Instance/Types/String/index.svelte
+++ src/components/Instance/Types/String/index.svelte
@@ -1,11 +1,48 @@
-<div>TODO</div>
-
 <script>
+
+    console.log($$props);
+  export let definition = {};
+  let minLength, maxLength, pattern;
+
+  ({ minLength, maxLength, pattern } = {...definition });
+
+  function validNumber(n) { 
+    if (isNaN(n) || n < 0 ) {
+        return "invalid";
+    } 
+    return "valid";
+  };
 
 </script>
 
 <style>
-div {
-  background-color: red;
-}
+  .invalid {
+    background-color: pink;
+  }
+  .valid {
+    background-color: white;
+  }
+  .default {
+    background-color: grey;
+  }
 </style>
+
+<div>
+
+  {#if minLength}
+    <div class="{validNumber(minLength)}" >minLength {minLength}</div>
+  {:else }
+    <div class="default" >minLength 0</div>
+  {/if}
+
+  {#if definition.maxLength}
+    <div class="{validNumber(maxLength)}" > maxLength {definition.maxLength}</div>
+  {:else }
+    <div class="default" >maxLength &infin</div>
+  {/if}
+
+  {#if definition.pattern} 
+    <div class="valid" >pattern {definition.pattern}</div>
+  {/if}
+
+</div>
