d1b78c7e82b342e15f46f43543a90361ee73333f
Yanick Champoux <yanick@babyl.dyndns.org>
2020-06-11 17:58:47 -0400

    add fetch_segment

--- src/routes/_schemas.js
+++ src/routes/_schemas.js
@@ -1,4 +1,5 @@
 import { writable, derived, get } from "svelte/store";
+import ptr from 'jsonpointer';
 
 /**
  * all the schemas the app is aware of
@@ -23,7 +24,8 @@ const selected_instance = derived(
 );
 
 const add_schema = (schema, url) => {
-  const id = schema.id || "file:///" + url;
+    // $id for >v4, id before
+  const id = schema['$id'] || schema.id || "file:///" + url;
 
   if(!schema.id) {
       schema = { ...schema, id };
@@ -34,10 +36,34 @@ const add_schema = (schema, url) => {
   selected_instance_url.update( $url => $url || id );
 };
 
+const fetch_segment = async (target,origin) => {
+    console.log({target,origin});
+
+    let schema_id, path;
+
+    if( target.indexOf('#') === 0 ) {
+        ([ schema_id ] = origin.split('#'));
+        path = target.substr(1);
+    }
+    else if( target.indexOf('#') === undefined ) {
+        schema_id = target;
+    }
+    else {
+        ([ schema_uri, path ] = target.split('#'));
+    }
+
+    const schema = get(schemas)[schema_id];
+
+    if(!path) return schema;
+
+    return ptr.get( schema, path );
+};
+
 export {
   schemas,
   add_schema,
   selected_instance_url,
   set_selected_instance_url,
   selected_instance,
+  fetch_segment,
 };
