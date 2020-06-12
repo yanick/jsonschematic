64a12e96f92c8fd3610221da5c7bb59696359c7b
Yanick Champoux <yanick@babyl.dyndns.org>
2020-06-12 16:06:36 -0400

    fix String test

--- src/components/Instance/Types/String/test.js
+++ src/components/Instance/Types/String/test.js
@@ -5,26 +5,27 @@ import { render } from "@testing-library/svelte";
 import String from ".";
 
 test("the String component says 'string'", () => {
-  const { getByText } = render(String, { });
+  const { getByText } = render(String, {});
 
   expect(getByText("string")).toBeInTheDocument();
 });
 
 test("default M-lengths", () => {
-  const { getByText } = render(String, { });
-  expect(getByText("minLength ≥ 0")).toBeInTheDocument();
-  expect(getByText("MaxLength",{exact: false})).toBeInTheDocument();
+  const { queryByText } = render(String, {});
+
+  expect(queryByText("≥ 0")).toBeNull();
+  expect(queryByText("MaxLength", { exact: false })).toBeNull();
 });
 
-test( "minLength prop", () => {
+test("minLength prop", () => {
+  const { getByText } = render(String, {
+    props: { definition: { minLength: 3, maxLength: 100, pattern: "*" } },
+  });
 
-  const { getByText } = render(String, {props: { definition: { minLength: 3, maxLength: 100, pattern: '*' }}});
-
-  expect(getByText("minLength ≥ 3")).toBeInTheDocument();
+  expect(getByText("≥ 3")).toBeInTheDocument();
 
   //renders &lt; and &gt; but not &infin; (it presents nothing in the infin case so
   //not the default
-  expect(getByText("maxLength ≤ 100")).toBeInTheDocument();
-  expect(getByText("value matches pattern \*")).toBeInTheDocument();
-
+  expect(getByText("≤ 100")).toBeInTheDocument();
+  expect(getByText("value matches pattern *")).toBeInTheDocument();
 });
