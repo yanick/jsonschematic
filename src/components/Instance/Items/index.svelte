f368f6da18e5858c021c881136f11523c7cd485a
Yanick Champoux <yanick@babyl.dyndns.org>
2020-06-12 15:46:18 -0400

    rework Items to be a table and use Property

--- src/components/Instance/Items/index.svelte
+++ src/components/Instance/Items/index.svelte
@@ -1,22 +1,24 @@
-<div>
-  items:
-
-  <Instance
-      href={items_href}
-      definition={items} />
-
-</div>
+<table>
+  <tbody>
+    <Property name="items">
+      <Instance href="" definition="{items}" />
+    </Property>
+  </tbody>
+</table>
 
 <script>
   export let items = {};
   export let href;
 
   let items_href;
-  $: items_href = [href,'items'].join('/');
+  $: items_href = [href, "items"].join("/");
 
-  import Instance from '../index.svelte';
-
+  import Instance from "../index.svelte";
+  import Property from "../Properties/Property.svelte";
 </script>
 
 <style>
+  div div {
+    margin-left: 1em;
+  }
 </style>
