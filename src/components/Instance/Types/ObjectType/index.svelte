366e7afb9bb64846323fb112fe74adcda64f9aa1
Yanick Champoux <yanick@babyl.dyndns.org>
2020-06-12 15:47:52 -0400

    add an Object type component

---
+++ src/components/Instance/Types/ObjectType/index.svelte
@@ -0,0 +1,5 @@
+<Type name="object" />
+
+<script>
+  import Type from "../Type";
+</script>
--- src/components/Instance/Types/index.svelte
+++ src/components/Instance/Types/index.svelte
@@ -1,29 +1,43 @@
+<div>
+
+  {#each types as type (type)}
+    <svelte:component this="{comp_for(type)}" {...definition} />
+  {/each}
+
+</div>
+
 <script>
   export let types = [];
   export let definition = {};
 
-  import Array from './Array/index.svelte';
-  import Number from './Number/index.svelte';
-  import Boolean from './Boolean/index.svelte';
+  import ArrayType from "./Array/index.svelte";
+  import NumberType from "./Number/index.svelte";
+  import StringType from "./String/index.svelte";
+  import BooleanType from "./Boolean/index.svelte";
+  import ObjectType from "./ObjectType/index.svelte";
 
   const type_components = {
-    array: Array,
-    number: Number,
-    boolean: Boolean
+    object: ObjectType,
+    array: ArrayType,
+    number: NumberType,
+    string: StringType,
+    boolean: BooleanType,
   };
 
-  const comp_for = type => type_components[type];
-
+  const comp_for = (type) => type_components[type];
 </script>
 
-<div>
-
-  {#each types as type (type)}
-    <svelte:component this={comp_for(type)} {...definition} />
-  {/each}
-
-  </div>
-
 <style>
+  div {
+    display: flex;
+  }
+  div > :global(div:after) {
+    content: "|";
+    margin-right: 0.5em;
+  }
+  div > :global(div:last-child:after) {
+    content: "";
+    margin-right: 0em;
+    color: var(--color-base01);
+  }
 </style>
-
