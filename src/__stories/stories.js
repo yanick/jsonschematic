a6e6d929fb200d428a49a474afb6d8b5111afc39
Yanick Champoux <yanick@babyl.dyndns.org>
2020-06-17 12:13:27 -0400

    chore: rename "Instances" stories to be "Examples"

--- src/__stories/stories.js
+++ src/__stories/stories.js
@@ -1,25 +1,35 @@
 import { withKnobs, object } from "@storybook/addon-knobs";
 
 import RefResolver from "./RefResolver.svelte";
-import Instance from '../components/Instance';
-import example1 from './example.json';
+import Instance from "../components/Instance";
+import example1 from "./example.json";
 
-import schema7 from '../../examples/json-schema-v7.json';
-import '../../static/global.css';
+import schema7 from "../../examples/json-schema-v7.json";
+import release_schema from "../../examples/release-schema.json";
 
-export default { title: "Instances", decorators: [withKnobs]};
+import "../../static/global.css";
+
+export default { title: "Examples", decorators: [withKnobs] };
 
 export const example_1 = () => ({
   Component: RefResolver,
   props: {
-      definition: object('definition', example1 )
+    definition: object("definition", example1),
   },
 });
 
 export const json_schema_v7 = () => ({
-    Component: Instance,
-    props: {
-        top_level: true,
-        definition: schema7,
-    }
-})
+  Component: Instance,
+  props: {
+    top_level: true,
+    definition: schema7,
+  },
+});
+
+export const release = () => ({
+  Component: Instance,
+  props: {
+    top_level: true,
+    definition: release_schema,
+  },
+});
