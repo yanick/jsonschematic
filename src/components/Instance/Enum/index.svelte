38729f2295cf4151db103452932baabac00b6f4a
Trey Bianchini <trey.bianchini@iinteractive.com>
2020-06-17 13:06:19 -0500

    examples enums

---
+++ src/components/Instance/Enum/index.svelte
@@ -0,0 +1,11 @@
+<Property name = "enum" href="{href}/enum"> <pre><code>[{enumeration}]</code></pre> </Property>
+
+<script>
+  export let enumeration = [];
+  export let href = "";
+
+  import Property from "../Properties/Property.svelte";
+</script>
+
+<style>
+</style>
