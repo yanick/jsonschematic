b57da52ad7f38b937d4d0ccc9276ff2cb6a2677a
Trey Bianchini <trey.bianchini@iinteractive.com>
2020-06-25 11:42:36 -0500

    refactored Integer

---
+++ src/components/Instance/Types/Integer/index.svelte
@@ -0,0 +1,7 @@
+<Number name="integer" {definition} />
+
+<script>
+  export let definition = {};
+
+  import Number from "../Number/index.svelte";
+</script>
--- src/components/Instance/Types/Number/test.js
+++ src/components/Instance/Types/Number/test.js
@@ -6,42 +6,55 @@ import { render } from "@testing-library/svelte";
 
 const Number = require("./index.svelte");
 
-tap.test("min/max", async (t) => {
-  const { getByText } = render(Number, {
-    definition: { minimum: 1, maximum: 12 },
-  });
-  t.ok(getByText("≥ 1"));
-  t.ok(getByText("≤ 12"));
+tap.test("number", async (t) => {
+  const { getByText } = render(Number, {});
+  t.ok(getByText("number"));
 });
 
-tap.test("x min/max ", async (t) => {
-  const { getByText } = render(Number, {
-    definition: { exclusiveMinimum: -1, exclusiveMaximum: 14 },
-  });
-  t.ok(getByText("> -1"));
-  t.ok(getByText("< 14"));
-});
+ranges(Number);
 
-tap.test("mixed ", async (t) => {
-  const { getByText } = render(Number, {
-    definition: { exclusiveMinimum: -9, maximum: 847 },
-  });
-  t.ok(getByText("> -9"));
-  t.ok(getByText("≤ 847"));
-});
+/* ranges suite */
 
-tap.test("v4 x mininum", async (t) => {
-  const { getByText } = render(Number, {
-    definition: { exclusiveMinimum: true, minimum: 19, maximum: 100 },
+function ranges(type) {
+  tap.test("min/max", async (t) => {
+    const { getByText } = render(type, {
+      definition: { minimum: 1, maximum: 12 },
+    });
+    t.ok(getByText("≥ 1"));
+    t.ok(getByText("≤ 12"));
   });
-  t.ok(getByText("> 19"));
-  t.ok(getByText("≤ 100"));
-});
 
-tap.test("v4 x maximum", async (t) => {
-  const { getByText } = render(Number, {
-    definition: { exclusiveMaximum: true, minimum: 17, maximum: 202 },
+  tap.test("x min/max ", async (t) => {
+    const { getByText } = render(type, {
+      definition: { exclusiveMinimum: -1, exclusiveMaximum: 14 },
+    });
+    t.ok(getByText("> -1"));
+    t.ok(getByText("< 14"));
   });
-  t.ok(getByText("≥ 17"));
-  t.ok(getByText("< 202"));
-});
+
+  tap.test("mixed ", async (t) => {
+    const { getByText } = render(type, {
+      definition: { exclusiveMinimum: -9, maximum: 847 },
+    });
+    t.ok(getByText("> -9"));
+    t.ok(getByText("≤ 847"));
+  });
+
+  tap.test("v4 x mininum", async (t) => {
+    const { getByText } = render(type, {
+      definition: { exclusiveMinimum: true, minimum: 19, maximum: 100 },
+    });
+    t.ok(getByText("> 19"));
+    t.ok(getByText("≤ 100"));
+  });
+
+  tap.test("v4 x maximum", async (t) => {
+    const { getByText } = render(type, {
+      definition: { exclusiveMaximum: true, minimum: 17, maximum: 202 },
+    });
+    t.ok(getByText("≥ 17"));
+    t.ok(getByText("< 202"));
+  });
+}
+
+export { ranges };
