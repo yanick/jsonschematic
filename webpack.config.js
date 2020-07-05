cddfc99cb03661e0cecec2f5178c04cc96a793e6
Yanick Champoux <yanick@babyl.dyndns.org>
2020-07-05 13:47:20 -0400

    going full webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,35 @@
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const path = require('path');
+const config = require('sapper/config/webpack.js');
+const pkg = require('./package.json');
 
-const mode = process.env.NODE_ENV || "development";
-const prod = mode === "production";
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const prod = !dev;
+
+const alias = { svelte: path.resolve('node_modules', 'svelte') };
+const extensions = ['.mjs', '.js', '.json', '.svelte', '.html'];
+const mainFields = ['svelte', 'module', 'browser', 'main'];
 
 module.exports = {
-  entry: {
-    bundle: ["./src/example.js"],
-  },
-  resolve: {
-    alias: {
-      svelte: path.resolve("node_modules", "svelte"),
-    },
-    extensions: [".mjs", ".js", ".svelte"],
-    mainFields: ["svelte", "browser", "module", "main"],
-  },
-  output: {
-    path: __dirname + "/docs/build",
-    filename: "[name].js",
-    chunkFilename: "[name].[id].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svelte$/,
-        use: {
-          loader: "svelte-loader",
-          options: {
-            emitCss: true,
-            hotReload: true,
-          },
-        },
-      },
-      {
+	client: {
+		entry: config.client.entry(),
+		output: config.client.output(),
+		resolve: { alias, extensions, mainFields },
+		module: {
+			rules: [
+				{
+					test: /\.(svelte|html)$/,
+					use: {
+						loader: 'svelte-loader',
+						options: {
+							dev,
+							hydratable: true,
+							hotReload: false // pending https://github.com/sveltejs/svelte/issues/2377
+						}
+					}
+				},{
         test: /\.css$/,
         use: [
           /**
@@ -42,14 +39,57 @@ module.exports = {
           prod ? MiniCssExtractPlugin.loader : "style-loader",
           "css-loader",
         ],
+      }
+			]
+		},
+		mode,
+		plugins: [
+			// pending https://github.com/sveltejs/svelte/issues/2377
+			// dev && new webpack.HotModuleReplacementPlugin(),
+			new webpack.DefinePlugin({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+		].filter(Boolean),
+		devtool: dev && 'inline-source-map'
+	},
+
+	server: {
+		entry: config.server.entry(),
+		output: config.server.output(),
+		target: 'node',
+		resolve: { alias, extensions, mainFields },
+		externals: Object.keys(pkg.dependencies).concat('encoding'),
+		module: {
+			rules: [
+                {
+        test: /\.css$/,
+        use: [
+          "css-loader",
+        ],
       },
-    ],
-  },
-  mode,
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-  ],
-  devtool: prod ? false : "source-map",
+				{
+					test: /\.(svelte|html)$/,
+					use: {
+						loader: 'svelte-loader',
+						options: {
+							css: false,
+							generate: 'ssr',
+							dev
+						}
+					}
+				}
+			]
+		},
+		mode: process.env.NODE_ENV,
+		performance: {
+			hints: false // it doesn't matter if server.js is large
+		}
+	},
+
+	serviceworker: {
+		entry: config.serviceworker.entry(),
+		output: config.serviceworker.output(),
+		mode: process.env.NODE_ENV
+	}
 };
