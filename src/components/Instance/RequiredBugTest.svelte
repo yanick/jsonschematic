72919f45943e06629f8d59e46536e6a12ca28ecf
Yanick Champoux <yanick@babyl.dyndns.org>
2020-07-09 13:37:37 -0400

    first try at reproducing the bug

---
+++ src/components/Instance/RequiredBugTest.svelte
@@ -0,0 +1,24 @@
+<script>
+    let definition = {
+        type: "object",
+        properties: {
+            foo: { type: 'string' },
+            bar: { type: 'string' },
+        },
+        required: [ 'foo' ],
+    }
+
+    const Instance = require('./index.svelte').default;
+
+    const change_def = () => {
+        definition = {
+            type: "object",
+            properties: {
+                quux: { type: 'string' },
+            }
+            };
+    };
+</script>
+
+<button on:click={change_def}>click me</button>
+<Instance {definition} />
