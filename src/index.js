247653b42b3a05a938bc46884e901c0752e1cd62
Yanick Champoux <yanick@babyl.ca>
2022-07-11 10:29:22 -0400

    export startJsonschematic

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import '../src/style/index.js';
 import JS from './components/Jsonschematic.svelte';
 
 export const Jsonschematic = JS;
+
+export function startJsonschematic(appId, ...schemas) {
+    const app = new JS({
+        target: document.getElementById(appId),
+        props: {
+            schemas,
+        },
+    });
+}
