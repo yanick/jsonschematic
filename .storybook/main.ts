d7162095654bff4d0270eaa326e20855d98db084
Yanick Champoux <yanick@babyl.ca>
2026-02-08 12:08:37 -0500

    sveltekit init

---
+++ .storybook/main.ts
@@ -0,0 +1,12 @@
+import type { StorybookConfig } from '@storybook/sveltekit';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.stories.@(js|ts|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-svelte-csf"
+  ],
+  "framework": "@storybook/sveltekit"
+};
+export default config;
