007a8726a69ffc86c350022f4c4a9c2a538dd0f5
Yanick Champoux <yanick@babyl.ca>
2026-02-09 09:08:13 -0500

    wip

---
+++ src/lib/components/Schema/Enum.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import Wrapper from './Wrapper.svelte';
+
+  const { schema = {} } = $props();
+
+  const enumeration = $derived(schema.enum ?? []);
+  const name = $derived(schema.constant ? 'const' : 'enum');
+</script>
+
+<Wrapper {name}>
+  <ul>
+    {#each enumeration as e}
+      <li>{e}</li>
+    {/each}
+  </ul>
+</Wrapper>
+
+<style>
+  ul {
+    list-style: none;
+    margin: 0px;
+    padding: 0px;
+    display: flex;
+  }
+  li:after {
+    content: ',';
+  }
+  li:last-child:after {
+    content: '';
+  }
+  li {
+    margin-right: 0.5em;
+  }
+  .filter input {
+    font-size: var(--font-scale-8);
+  }
+  .filter .filter_box {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+</style>
---
+++ src/lib/components/Schema/Wrapper.svelte
@@ -0,0 +1,20 @@
+<script>
+  const { name, required, children } = $props();
+</script>
+
+<div class="property-name" class:required>{name}</div>
+
+<div class="property_def">
+  {@render children?.()}
+</div>
+
+<style>
+  .property-name {
+    margin-left: 1em;
+  }
+  .required:before {
+    content: '!';
+    font-weight: bold;
+    color: var(--color-red);
+  }
+</style>
