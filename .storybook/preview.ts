eaab54ff166f07aa6a2a480fe882a125bd35e927
Yanick Champoux <yanick@babyl.ca>
2026-02-09 14:02:38 -0500

    add picocss to the Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,15 @@
 import type { Preview } from '@storybook/sveltekit'
+import '@picocss/pico/css/pico.css';
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
