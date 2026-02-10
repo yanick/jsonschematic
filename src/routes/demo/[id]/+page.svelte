59e8679831fe87e1635004b745c2d5cca8b4d4ee
Yanick Champoux <yanick@babyl.ca>
2026-02-10 17:42:28 -0500

    wip

--- src/routes/demo/[id]/+page.svelte
+++ src/routes/demo/[id]/+page.svelte
@@ -2,12 +2,29 @@
   import examples, { scrubExample } from '$lib/examples.js';
   import u from '@yanick/updeep';
   import Schema from '$lib/components/Schema.svelte';
+  import slug from 'slug';
 
   const { data } = $props();
 
-  const schema = $derived(scrubExample(examples.find(u.matches({ _id: data.id }))));
+  const schema = $derived(examples.find((eg) => slug(eg.title) == data.id));
 </script>
 
 <main class="container">
-  <Schema {schema} />
+  <div>
+    <Schema {schema} />
+  </div>
+  <div>
+    <pre>{JSON.stringify(schema, null, 2)}</pre>
+  </div>
 </main>
+
+<style>
+  main {
+    display: flex;
+    column-gap: 2em;
+  }
+
+  main > * {
+    flex: 1;
+  }
+</style>
