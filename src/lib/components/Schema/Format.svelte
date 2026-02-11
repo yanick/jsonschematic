d5696d2852cfa31156d03bdad3ec37f8ce67d12d
Yanick Champoux <yanick@babyl.ca>
2026-02-11 11:11:29 -0500

    format

---
+++ src/lib/components/Schema/Format.svelte
@@ -0,0 +1,8 @@
+<script>
+  const { schema = {} } = $props();
+  import formats from './formats.js';
+</script>
+
+<li>
+  format: <span data-tooltip={formats[schema.format]}>{schema.format}</span>
+</li>
---
+++ src/lib/components/Schema/formats.js
@@ -0,0 +1,40 @@
+export default {
+  "date-time":
+    "Date and time together, for example, `2018-11-13T20:20:39+00:00`",
+  time: "Time, for example, 20:20:39+00:00",
+  date: "Date, for example, 2018-11-13",
+  email:
+    "Internet email address, see <a href='https://tools.ietf.org/html/rfc5322#section-3.4.1'>RFC 5322, section 3.4.1</a>",
+  "idn-email":
+    'The internationalized form of an Internet email address, see <a href="https://tools.ietf.org/html/rfc6531">RFC 6531</a>',
+  hostname:
+    'Internet host name, see <a href="https://tools.ietf.org/html/rfc1034#section-3.1">RFC 1034, section 3.1</a>',
+  "idn-hostname":
+    'New in draft 7 An internationalized Internet host name, see <a href="https://tools.ietf.org/html/rfc5890#section-2.3.2.3">RFC5890, section 2.3.2.3</a>',
+  ipv4:
+    'IPv4 address, according to dotted-quad ABNF syntax as defined in <a href="https://tools.ietf.org/html/rfc2673#section-3.2">RFC 2673, section 3.2</a>',
+  ipv6:
+    'IPv6 address, as defined in <a href="https://tools.ietf.org/html/rfc2373#section-2.2">RFC 2373, section 2.2</a>',
+  uri:
+    'A universal resource identifier (URI), according to <a href="https://tools.ietf.org/html/rfc3986">RFC3986</a>',
+  "uri-reference":
+    'New in draft 6 A URI Reference (either a URI or a relative-reference), according to <a href="https://tools.ietf.org/html/rfc3986#section-4.1">RFC3986, section 4.1</a>',
+
+  iri:
+    'The internationalized equivalent of a “uri”, according to <a href="https://tools.ietf.org/html/rfc3987">RFC3987</a>',
+
+  "iri-reference":
+    'The internationalized equivalent of a “uri-reference”, according to <a href="https://tools.ietf.org/html/rfc3987">RFC3987</a>',
+
+  "uri-template":
+    'A URI Template (of any level) according to <a href="https://tools.ietf.org/html/rfc6570">RFC6570.</a>',
+
+  "json-pointer":
+    'A JSON Pointer, according to <a href="https://tools.ietf.org/html/rfc6901">RFC6901</a>',
+
+  "relative-json-pointer":
+    'A <a href="https://tools.ietf.org/html/draft-handrews-relative-json-pointer-01">relative JSON pointer</a>',
+
+  regex:
+    'A regular expression, which should be valid according to the <a href="https://www.ecma-international.org/publications/files/ECMA-ST/Ecma-262.pdf">ECMA 262 dialect</a>',
+};
