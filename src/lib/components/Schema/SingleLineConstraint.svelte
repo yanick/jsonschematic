1ee9355bd2a53132a94cefedc7208d6a99d272f7
Yanick Champoux <yanick@babyl.ca>
2026-02-11 10:03:25 -0500

    string min/maxlength

---
+++ src/lib/components/Schema/SingleLineConstraint.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Constraint from './Constraint.svelte';
+  const { schema = {}, children = undefined } = $props();
+</script>
+
+<Constraint>
+  <ul>{@render children?.()}</ul>
+</Constraint>
+
+<style>
+  ul {
+    margin: 0px;
+    padding: 0px;
+    display: flex;
+  }
+  ul :global(li:after) {
+    content: ',';
+  }
+  ul :global(li:last-child:after) {
+    content: '';
+  }
+  ul :global(li) {
+    font-style: italic;
+    list-style: none;
+    margin-right: 0.5em;
+  }
+</style>
