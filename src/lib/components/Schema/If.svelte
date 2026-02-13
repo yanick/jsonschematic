4c82055d83fe3c98c1ae2226a4e8b3abe0592074
Yanick Champoux <yanick@babyl.ca>
2026-02-13 13:55:54 -0500

    If and SomeOf

---
+++ src/lib/components/Schema/If.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Schema from '../Schema.svelte';
+  import Constraint from './Constraint.svelte';
+
+  const { schema = {} } = $props();
+</script>
+
+<Constraint label="if">
+  <Schema schema={schema.if} />
+  {#each ['then', 'else'] as keyword (keyword)}
+    {#if schema[keyword]}
+      <Constraint label={keyword}><Schema schema={schema[keyword]} /></Constraint>
+    {/if}
+  {/each}
+</Constraint>
