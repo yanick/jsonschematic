5f7599cc3302242510948ff07195dfe99ecf9f11
Yanick Champoux <yanick@babyl.ca>
2026-02-16 15:25:26 -0500

    working on example

---
+++ src/routes/examples/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import release from './release-schema.json';
+  import Schema from '$lib/components/Schema.svelte';
+</script>
+
+<div class="container">
+  <Schema schema={release} />
+</div>
