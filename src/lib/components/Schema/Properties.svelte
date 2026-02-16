1e066861d235a1172063456e4c7199788fb93397
Yanick Champoux <yanick@babyl.ca>
2026-02-16 14:48:28 -0500

    properties

---
+++ src/lib/components/Schema/Properties.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Constraint from './Constraint.svelte';
+  import Schema from '../Schema.svelte';
+
+  const { properties = {} } = $props();
+</script>
+
+<Constraint label="properties">
+  {#each Object.entries(properties) as [name, schema] (name)}
+    <Constraint label={name}>
+      <Schema {schema} />
+    </Constraint>
+  {/each}
+</Constraint>
